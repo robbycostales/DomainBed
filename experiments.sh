b5f6d9f954dc9a8644ff315de575282a61eb093c
Robby Costales <robbycostales@gmail.com>
2022-05-04 15:03:08 -0700

    Buying more crash insurance

--- experiments.sh
+++ experiments.sh
@@ -9,56 +9,57 @@ echo "EXECUTING GPU $GPU CODE"
 CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
     --data_dir=./domainbed/data/\
     --hparams='{"resnet18": 1}'\
-    --output_dir=./output_pacs_coral_t3_adapt_hf-0.8_al-1.0\
+    --output_dir=./output_pacs_coral_t3_adapt_hf-0.8_al-0.2\
     --algorithm CORAL\
     --dataset PACS\
     --test_env 3\
     --task domain_adaptation\
     --uda_holdout_fraction 0.8\
-    --adapt_lambda 1.0
+    --adapt_lambda 0.2
 
 CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
     --data_dir=./domainbed/data/\
     --hparams='{"resnet18": 1}'\
-    --output_dir=./output_pacs_coral_t3_adapt_hf-0.9_al-1.0\
+    --output_dir=./output_pacs_coral_t3_adapt_hf-0.8_al-0.1\
     --algorithm CORAL\
     --dataset PACS\
     --test_env 3\
     --task domain_adaptation\
-    --uda_holdout_fraction 0.9\
-    --adapt_lambda 1.0
-
-fi
-
-# GPU 1
-
-GPU=1
-
-if [ $GPU == $1 ]; then
-
-echo "EXECUTING GPU $GPU CODE"
-
-CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
-    --data_dir=./domainbed/data/\
-    --hparams='{"resnet18": 1}'\
-    --output_dir=./output_pacs_coral_t3_adapt_hf-0.9_al-0.5\
-    --algorithm CORAL\
-    --dataset PACS\
-    --test_env 3\
-    --task domain_adaptation\
-    --uda_holdout_fraction 0.9\
-    --adapt_lambda 0.5
-
-CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
-    --data_dir=./domainbed/data/\
-    --hparams='{"resnet18": 1}'\
-    --output_dir=./output_pacs_coral_t3_adapt_hf-0.9_al-0.1\
-    --algorithm CORAL\
-    --dataset PACS\
-    --test_env 3\
-    --task domain_adaptation\
-    --uda_holdout_fraction 0.9\
+    --uda_holdout_fraction 0.8\
     --adapt_lambda 0.1
+
+CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
+    --data_dir=./domainbed/data/\
+    --hparams='{"resnet18": 1}'\
+    --output_dir=./output_pacs_coral_t3_adapt_hf-0.8_al-0.05\
+    --algorithm CORAL\
+    --dataset PACS\
+    --test_env 3\
+    --task domain_adaptation\
+    --uda_holdout_fraction 0.8\
+    --adapt_lambda 0.05
+
+CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
+    --data_dir=./domainbed/data/\
+    --hparams='{"resnet18": 1}'\
+    --output_dir=./output_pacs_coral_t3_adapt_hf-0.8_al-0.3\
+    --algorithm CORAL\
+    --dataset PACS\
+    --test_env 3\
+    --task domain_adaptation\
+    --uda_holdout_fraction 0.8\
+    --adapt_lambda 0.3
+
+CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
+    --data_dir=./domainbed/data/\
+    --hparams='{"resnet18": 1}'\
+    --output_dir=./output_pacs_coral_t3_adapt_hf-0.8_al-0.01\
+    --algorithm CORAL\
+    --dataset PACS\
+    --test_env 3\
+    --task domain_adaptation\
+    --uda_holdout_fraction 0.8\
+    --adapt_lambda 0.01
 
 fi
 
@@ -73,21 +74,64 @@ echo "EXECUTING GPU $GPU CODE"
 CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
     --data_dir=./domainbed/data/\
     --hparams='{"resnet18": 1}'\
-    --output_dir=./output_pacs_coral_t3_adapt_hf-0.9_al-0.0\
-    --algorithm CORAL\
-    --dataset PACS\
-    --test_env 3\
-    --task domain_adaptation\
-    --uda_holdout_fraction 0.9\
-    --adapt_lambda 0.0
-
-CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
-    --data_dir=./domainbed/data/\
-    --hparams='{"resnet18": 1}'\
     --output_dir=./output_pacs_coral_t3_gen\
     --algorithm CORAL\
     --dataset PACS\
     --test_env 3
+
+CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
+    --data_dir=./domainbed/data/\
+    --hparams='{"resnet18": 1}'\
+    --output_dir=./output_pacs_coral_t3_adapt_hf-0.4_al-0.1\
+    --algorithm CORAL\
+    --dataset PACS\
+    --test_env 3\
+    --task domain_adaptation\
+    --uda_holdout_fraction 0.4\
+    --adapt_lambda 0.1
+
+CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
+    --data_dir=./domainbed/data/\
+    --hparams='{"resnet18": 1}'\
+    --output_dir=./output_pacs_coral_t3_adapt_hf-0.1_al-0.1\
+    --algorithm CORAL\
+    --dataset PACS\
+    --test_env 3\
+    --task domain_adaptation\
+    --uda_holdout_fraction 0.1\
+    --adapt_lambda 0.1
+
+CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
+    --data_dir=./domainbed/data/\
+    --hparams='{"resnet18": 1}'\
+    --output_dir=./output_pacs_coral_t3_adapt_hf-0.05_al-0.1\
+    --algorithm CORAL\
+    --dataset PACS\
+    --test_env 3\
+    --task domain_adaptation\
+    --uda_holdout_fraction 0.05\
+    --adapt_lambda 0.1
+
+CUDA_VISIBLE_DEVICES=$GPU python3 -m domainbed.scripts.train \
+    --data_dir=./domainbed/data/\
+    --hparams='{"resnet18": 1}'\
+    --output_dir=./output_pacs_coral_t3_adapt_hf-0.01_al-0.1\
+    --algorithm CORAL\
+    --dataset PACS\
+    --test_env 3\
+    --task domain_adaptation\
+    --uda_holdout_fraction 0.01\
+    --adapt_lambda 0.1
+
+fi
+
+# GPU 1
+
+GPU=1
+
+if [ $GPU == $1 ]; then
+
+echo "EXECUTING GPU $GPU CODE"
 
 fi
 
